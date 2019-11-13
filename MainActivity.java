09f36a2377129b3734229e4486bab64da9d21b7f
juiceboxbot <ellieyong1@gmail.com>
2019-11-13 15:38:12 +0000

    Week 7 lab, playing about with light sensors

--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.juiceboxbot.y3840030mobileandroid;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager s_mgr;
+    private Sensor s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s_mgr = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        s = s_mgr.getDefaultSensor(Sensor.TYPE_LIGHT);
+        s_mgr.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int value) {
 
     }
 
+    public void onSensorChanged(SensorEvent event) {
+        ((TextView)findViewById(R.id.textView)).setText("SENSOR DATE:\n" + String.valueOf(event.values[0]));
+
+        if(event.values[0] >= s_mgr.LIGHT_FULLMOON) {
+            //set Image to show up
+            findViewById(R.id.imageView).setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        s_mgr.unregisterListener(this);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
