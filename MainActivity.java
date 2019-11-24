bbc6e627823d5b220116fe089389cf200ed4e13c
juiceboxbot <ellieyong1@gmail.com>
2019-11-24 19:25:35 +0000

    created a Function class which holds all code for external web calling such as network checking and executing URLs.
    Changed MainActivity by adding functionality to intake complex JSON.

--- MainActivity.java
+++ MainActivity.java
@@ -12,7 +12,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,11 +24,33 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.example.juiceboxbot.y3840030mobileandroid.Function;
+
+
+/**
+ * Created by juiceboxbot on 15/11/2019.
+ */
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager s_mgr;
     private Sensor s;
+    public String NEWS_SOURCE;
+    public String API_KEY = "820dc531-f4bc-4ae6-8a66-e1e8013b47d0";
+    public  Function func;
+
+    ArrayList<HashMap<String, String>> dataList = new ArrayList<>();
+    static final String KEY_AUTHOR = "author";
+    static final String KEY_TITLE = "title";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_URL = "url";
+    static final String KEY_URLTOIMAGE = "urlToImage";
+    static final String KEY_PUBLISHEDAT = "publishedAt";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,29 +59,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         s_mgr = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         s = s_mgr.getDefaultSensor(Sensor.TYPE_LIGHT);
         s_mgr.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        if (func.isNetworkAvailable(getApplicationContext())) {
+            downloadNews newsTask = new downloadNews();
+            newsTask.execute();
+        } else {
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
     }
 
 
-    public void getNews(View v) {
+    public String doInBackground(View v) {
         //new getNewsFacts().execute("820dc531-f4bc-4ae6-8a66-e1e8013b47d0");
 
         //URL url = new URL("http://eventregistry.org/api/v1/event/getEvents/");
         //HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 //https://newsapi.org/v2/top-headlines?country=us&apiKey=034c261d12784990b41d4ef4f235ed1d
-        new downloadNews().execute("https://newsapi.org/v2/top-headlines" + "country=us&" + "apiKey=034c261d12784990b41d4ef4f235ed1d");
+        new downloadNews().execute("https://newsapi.org/v2/top-headlines?" +
+                                    "country=uk&" +
+                                    "apiKey=034c261d12784990b41d4ef4f235ed1d");
 
+        String xml = func.excuteGet("https://newsapi.org/v2/articles?source=" + NEWS_SOURCE + "&sourtBy=top&apiKey=" + API_KEY);
+        return xml;
+
+        //new downloadNews().execute("https://newsapi.org/v2/top-headlines?country=us&apiKey=034c261d12784990b41d4ef4f235ed1d");
         //sourceLocationUri  array[string]
-    //includeEventLocation  boolean
-        // includeStoryLocation boolean
 
         //Conncct api from web to local.
-        //820dc531-f4bc-4ae6-8a66-e1e8013b47d0
+        //
         //
     }
 
     public void setNews(String new_news) {
         TextView tv = (TextView) findViewById(R.id.textView2);
         tv.setText(new_news);
+
+/*   ListNewsAdapter adapter = new ListNewsAdapter(MainActivity.this, dataList);
+        listNews.setAdapter(adapter);
+
+        listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent i = new Intent(MainActivity.this, DetailsActivity.class);
+                i.putExtra("url", dataList.get(+position).get(KEY_URL));
+                startActivity(i);
+*/
     }
 
     public void onAccuracyChanged(Sensor sensor, int value) {
@@ -74,6 +121,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         else{
             findViewById(R.id.imageView).setVisibility(View.INVISIBLE);
         }
+
+
     }
 
     @Override
@@ -139,21 +188,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         @Override
         protected void onPostExecute(String result) {
-            if(result.length()==0) {
+            if(result.length()== 0) { //Checking if the string comes back empty or not
                 setNews("ERROR :(");
                 return;
             }
 
-            String news;
             try {
-                JSONObject json = new JSONObject(result);
-                news = json.getString("news");
+                JSONObject jsonResponse = new JSONObject(result);
+                JSONArray jsonArray = jsonResponse.optJSONArray("articles");
             }
             catch(JSONException e) {
-                news = e.getLocalizedMessage();
+                Toast.makeText(getApplicationContext(), "Unexpected error", Toast.LENGTH_SHORT).show();
             }
 
-            setNews(news);
+            //setNews(news);
         }
     }
 }
